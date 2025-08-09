8c50f3576fb877ca840bf31b3694b622c8c7e9b2
miro-ring <miro.ring@kakaocorp.com>
2025-08-09 12:30:10 +0900

    fix: change background

--- js/skyHeight.js
+++ js/skyHeight.js
@@ -1,6 +1,19 @@
 function setVhProperty() {
   const vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  const fixedWidth = window.screen.width;
+  const fixedHeight = window.screen.height;
+
+  // CSS 변수로 고정 크기 설정
+  document.documentElement.style.setProperty(
+    "--fixed-width",
+    `${fixedWidth}px`
+  );
+  document.documentElement.style.setProperty(
+    "--fixed-height",
+    `${fixedHeight}px`
+  );
 }
 
 // 초기 설정
