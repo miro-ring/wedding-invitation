dfe5ba812f41011a0f9fdc8b2fdb20c6ceb0605f
miro-ring <miro.ring@kakaocorp.com>
2025-08-24 21:05:18 +0900

    feat: set lazy load for images

--- js/gallery.js
+++ js/gallery.js
@@ -4,8 +4,8 @@ new Swiper(".gallery-thumbs", {
   slidesPerView: 3,
   centeredSlides: true,
   slideToClickedSlide: true,
-  preloadImages: true,
-  updateOnImagesReady: true,
+  preloadImages: false, // 이미지 미리 로드 비활성화 (성능 개선)
+  lazy: true, // lazy loading 활성화
   watchSlidesProgress: true,
 
   pagination: {
